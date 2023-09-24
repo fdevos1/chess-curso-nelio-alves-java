4d10b478aef822208c02b4fe861065f56aaa2c8b
fdevos1 <felipe.devosm@gmail.com>
2023-09-24 17:47:35 -0300

    added rook moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -31,6 +31,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		
 		return (ChessPiece) capturedPiece;
@@ -44,6 +45,8 @@ public class ChessMatch {
 		return capturedPiece;
 	}
 	
+	
+	
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
@@ -54,6 +57,12 @@ public class ChessMatch {
 		}
 	}
 	
+	
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position");
+		}
+	}
 	
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
